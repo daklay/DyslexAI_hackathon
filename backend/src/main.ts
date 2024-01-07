e2764401843a96a38aaa2fc1233bd03b4595ce36
Yassineanoire <anoire.yassine@gmail.com>
2024-01-07 11:25:38 +0100

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://p8ygx8-3000.csb.app',
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
- });
+  });
   await app.listen(3000,'0.0.0.0');
 }
 bootstrap();
